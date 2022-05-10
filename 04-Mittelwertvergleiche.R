b88bdc6eb8530cbf04212e889a5d77a29919e870
Michael Scharkow <michael@underused.org>
2022-05-10 11:49:58 +0200

    Fixes

--- 04-Mittelwertvergleiche.R
+++ 04-Mittelwertvergleiche.R
@@ -3,7 +3,6 @@
 # install.packages(c("tidyverse", "report", "marginaleffects"))
 
 library(tidyverse)
-library(report)
 library(marginaleffects)
 
 # Mittelwertvergleiche
@@ -15,39 +14,55 @@ library(marginaleffects)
 kuempel2019 = haven::read_sav("data/Kuempel_2019.sav") %>%
   mutate(Kurationsmodus_SNS = as_factor(Kurationsmodus_SNS), # Gruppierungsvariable als Factor
          Kurationsmodus_SNS = relevel(Kurationsmodus_SNS, "Chronik")) %>%  # Referenzgruppe
-  haven::zap_labels() # Probleme mit Variablenlabels, daher loeschen
+  haven::zap_labels() %>%
+  mutate(Modus_Tag = ifelse(Kurationsmodus_SNS=="Tag", "1 (ja)", "0 (nein)")) %>%
+  select(Modus = Kurationsmodus_SNS, Modus_Tag, Rezeptionswahrscheinlichkeit)
+
+kuempel2019
 
 # Gruppenmittelwerte
 kuempel2019 %>%
-  report_sample(select = "Rezeptionswahrscheinlichkeit", group_by = "Kurationsmodus_SNS")
+  group_by(Modus_Tag) %>%
+  summarise(n = n(), m = mean(Rezeptionswahrscheinlichkeit), sd = sd(Rezeptionswahrscheinlichkeit))
+
+# T-Test in R
+t.test(Rezeptionswahrscheinlichkeit ~ Modus_Tag, data = kuempel2019, var.equal = TRUE)
+
+model_lm1 = lm(Rezeptionswahrscheinlichkeit ~ Modus_Tag, data = kuempel2019)
+summary(model_lm1)
+
+# Vorhergesagte Mittelwerte
+marginalmeans(model_lm1) %>%
+  summary()
+
 
 # ANOVA in R
-model_anova = aov(Rezeptionswahrscheinlichkeit ~ Kurationsmodus_SNS, kuempel2019)
+model_anova = aov(Rezeptionswahrscheinlichkeit ~ Modus, kuempel2019)
 summary(model_anova)
 
-# Zusammenfassung mit report Paket
-report_table(model_anova)
-report_text(model_anova)
 
 # Randmittel und Kontraste mit dem modelbased Paket
-marginalmeans(model_anova) %>% summary()
-comparisons(model_anova, contrast_factor = "pairwise") %>% summary()
+marginalmeans(model_anova) %>%
+  summary()
+comparisons(model_anova, contrast_factor = "pairwise") %>%
+  summary()
 
 # Lineares Modell mit Factor (automatische Dummy-Codierung)
-model_lm1 = lm(Rezeptionswahrscheinlichkeit ~ Kurationsmodus_SNS, kuempel2019)
-summary(model_lm1)
-report_table(model_lm1)
+model_lm2 = lm(Rezeptionswahrscheinlichkeit ~ Modus, kuempel2019)
+summary(model_lm2)
 
 # Randmittel und Kontraste mit dem modelbased Paket
-marginalmeans(model_lm1)
-comparisons(model_lm1, contrast_factor = "pairwise") %>% summary()
+marginalmeans(model_lm2)
+comparisons(model_lm2, contrast_factor = "pairwise") %>%
+  summary()
 
 # Bonus: Grafik
-marginalmeans(model_lm1)  %>%
+marginalmeans(model_lm2)  %>%
   tidy() %>%
   ggplot(aes(x = value, y = estimate,
              ymin = conf.low, ymax = conf.high)) +
   geom_pointrange()+
   coord_cartesian(ylim = c(1,5))+
-  labs(x = "Kurationsmodus", y = "Rezeptionswahrscheinlichkeit")+
+  labs(x = "Kurationsmodus", y = "Rezeptionswahrscheinlichkeit",
+       title ="Vorhergesagte Rezeptionswahrscheinlichkeit nach Kurationsmodus")+
   theme_bw()
