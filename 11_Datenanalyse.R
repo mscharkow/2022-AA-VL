d1ac1ce4efb4d85ca660d1af750626f5883493c4
Michael Scharkow <michael@underused.org>
2022-07-05 09:13:35 +0200

    Add 11.

---
+++ 11_Datenanalyse.R
@@ -0,0 +1,63 @@
+# VL Anwendungsorientierte Analyseverfahren, Prof. Dr. Michael Scharkow
+# Wenn RStudio Ihnen nicht vorschlaegt die Pakete zu installieren:
+# install.packages(c("tidyverse", "report", "marginaleffects", "lme4))
+
+library(tidyverse)
+library(report)
+library(lme4)
+library(marginaleffects)
+
+theme_set(theme_bw())
+
+lazarus = haven::read_sav("data/Lazarus_2021.sav") %>%
+  mutate(country = as_factor(country),
+         educ_fact = as_factor(educ_fact))
+lazarus
+summary(lazarus)
+
+# FF1: Länderunterschiede
+m1 = lm(vacc ~ country, data = lazarus)
+summary(m1)
+
+# H1: Geschlecht
+m2 = lm(vacc ~ female, data = lazarus)
+summary(m2)
+
+# H3: Alter
+m3 = lm(vacc ~ age, data = lazarus)
+summary(m3)
+
+# Kombiniert
+
+m4 = lm(vacc ~ age + female + country, data = lazarus)
+summary(m4)
+report_table(m4)
+
+# Modellvergleich (Länder vs. Länder + Soziodemographie)
+anova(m1, m4)
+
+# Vorhergesagte Werte bei Ländern
+marginaleffects::marginalmeans(m4, variables = "country") %>%
+  summary()
+
+marginaleffects::marginalmeans(m4, variables = "country") %>%
+  ggplot(aes(x = reorder(value, marginalmean), y = marginalmean,
+             ymin = conf.low, ymax = conf.high))+
+  geom_pointrange()+
+  coord_flip()+
+  labs(x = "", y = "Impfbereitschaft (95%-CI)")
+
+# Vorhergesagte Werte bei Geschlecht
+marginaleffects::marginalmeans(m4, variables = "female") %>%
+  summary()
+
+marginaleffects::marginalmeans(m4, variables = "female") %>%
+  ggplot(aes(x = reorder(value, marginalmean), y = marginalmean,
+             ymin = conf.low, ymax = conf.high))+
+  geom_pointrange()+
+  labs(x = "", y = "Impfbereitschaft (95%-CI)")
+
+# BONUS: H3
+m5 = lmer(vacc ~ trustngov + (1|country), data = lazarus)
+summary(m5)
+report::report_table(m5)
